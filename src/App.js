6f849d7fa3852e82c25047e951ccb2acc86981ec
Anirudh Banerjee <imanirudh.dev@gmail.com>
2020-10-27 22:52:32 +0530

    installed react-icons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-
-import './App.css';
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-     Hello
-    </div>
-  );
+  return <div className='App'>
+    <h1>hello</h1>
+  </div>
 }
 
-export default App;
+export default App
